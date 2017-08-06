0507cdbc4900bd15f5249918ca1262b23bd4e020
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-06 19:51:24 +0530

    changes in data will bre reflected back to ui and fields are made to be cleared after the entry

--- app.js
+++ app.js
@@ -83,7 +83,9 @@ var UIController=(function()
           	inputType:'.add__type',
           	inputDescription:'.add__description',
           	inputValue:'.add__value',
-          	inputBtn:'.add__btn'
+          	inputBtn:'.add__btn',
+          	incomeContainer:'.income__list',
+          	expensesContainer:'.expenses__list'
           };
           return{
           
@@ -104,13 +106,47 @@ var UIController=(function()
 	               },
 	               addItemList:function(obj,type)
 	               {
+                     var html,newHtml,element;
                      //create html string with placeholder text
+                     if(type==='inc')
+                     {
+                          element=DOMstrings.incomeContainer;
+                          html='<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                     }
+                     else if(type==='exp')
+                     {
+                          element=DOMstrings.expensesContainer;
+                          html='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                     }
+
                      //replace the placeholder text with some actual data
-                     //insert the html into dom
+
+                     newHtml=html.replace('%id%',obj.id);
+                     newHtml=newHtml.replace('%description%',obj.description);
+                     newHtml=newHtml.replace('%value%',obj.value);
+                    
+                    
+                     //insert the html into dom (using insertAdjacentHTML)
+
+                     document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+	               },
+	               clearFields:function()
+	               {
+                      var fields,fieldsArr;
+                      fields=document.querySelector(DOMstrings.inputDescription +','+DOMstrings.inputValue);
+                      //the problem is fields is a list not array
+                      //we can not use slice function on list(fields.slice ----it is wrong) it for arrays
+                      //what we can do is to use call function (see below)
+                      fieldsArr=Array.prototype.slice.call(fields);
+                      fieldsArr.forEach(function(current,index,array)
+                      	{
+                            current.value="";
+                      	});
+                      fieldsArr[0].focus();
 	               },
 	               getDOMstrings:function()
 	               {
-	               	 return DOMstrings;
+	               	   return DOMstrings;
 	               }
 
           };
@@ -119,6 +155,7 @@ var UIController=(function()
 
 	})();
 //GLOBAL APP CONTROLLER
+
 var controller=(function(budgetCtrl,UICtrl)
 	{
         // var z=budgetCtrl.publicTest(5);
@@ -152,8 +189,11 @@ var controller=(function(budgetCtrl,UICtrl)
         		//2.add the item to budget controller
         		newItem=budgetCtrl.addItem(input.type,input.description,input.value);
         		//3.add the new item to user interface
-        		
+        		UICtrl.addItemList(newItem,input.value);
+        		//3* clear the input fields
+        		UICtrl.clearFields();
         		//4.calculate the budget
+
         		//5.need to display the budget on UI
         };
 
