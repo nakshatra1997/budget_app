e63364a06ef50d60dad29a4e7146a34530b4a9c2
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2017-08-06 23:19:00 +0530

    budget,income and expenses are visible to ui now

--- app.js
+++ app.js
@@ -127,7 +127,11 @@ var UIController=(function()
           	inputValue:'.add__value',
           	inputBtn:'.add__btn',
           	incomeContainer:'.income__list',
-          	expensesContainer:'.expenses__list'
+          	expensesContainer:'.expenses__list',
+          	budgetLabel:'.budget__value',
+          	incomeLabel:'.budget__income--value',
+          	expensesLabel:'.budget__expenses--value',
+          	percentageLabel:'.budget__expenses--percentage'
           };
           return{
           
@@ -186,6 +190,21 @@ var UIController=(function()
                       	});
                       fieldsArr[0].focus();
 	               },
+	               displayBudget:function(obj)
+	               {
+                       document.querySelector(DOMstrings.budgetLabel).textContent=obj.budget;
+                       document.querySelector(DOMstrings.incomeLabel).textContent=obj.totalInc;
+                       document.querySelector(DOMstrings.expensesLabel).textContent=obj.totalExp;
+                       if(obj.percentage>0)
+                       {
+                         	document.querySelector(DOMstrings.percentageLabel).textContent=obj.percentage+'%';
+                       }
+                       else
+                       {
+                            document.querySelector(DOMstrings.percentageLabel).textContent='---';
+                       }
+	               },
+
 	               getDOMstrings:function()
 	               {
 	               	   return DOMstrings;
@@ -229,7 +248,7 @@ var controller=(function(budgetCtrl,UICtrl)
                 //2. return the budget
                 var budget=budgetCtrl.getBudget();
         		//3.need to display the budget on UI
-        		console.log(budget);
+        		UICtrl.displayBudget(budget);
         };
 
         var ctrlAddItem=function()
@@ -255,6 +274,12 @@ var controller=(function(budgetCtrl,UICtrl)
           {
           	console.log('app has started');
           	setupEventListeners();
+          	UICtrl.displayBudget({
+               	  budget:0,
+                  totalInc:0,
+                  totalExp:0,
+                  percentage:-1
+               });
           };
         // return
         // {
